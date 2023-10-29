656f1964f576a21e7cda38ba94ec0321b1dddad8
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-10-28 21:53:22 -0700

    Client/webpack.config.js - comment clean up

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,12 +22,12 @@ module.exports = () => {
         title: "J.A.T.E.",
         long_title: "Just Another Text Editor",
       }),
-      // Injects custom service worker
+      // Inject custom service worker.
       new InjectManifest({
-        swSrc: "./src-sw.js", // Get it from client folder
-        swDest: "src-sw.js", // Put it into the root of the build folder (dist)
+        swSrc: "./src-sw.js", // Get it from client folder.
+        swDest: "src-sw.js", // Put it into the root of the build folder (dist).
       }),
-      // Creates a manifest.json file.
+      // Create a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
@@ -36,13 +36,13 @@ module.exports = () => {
         description: "Leave a note!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
-        start_url: "/", // http://localhost:3001/ <- the last '/' is what the start_url is
+        start_url: "/", // http://localhost:3001/ <- The last '/' is what the start_url is.
         publicPath: "./",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"), // name of the folders; icons folder go into assets folder
+            destination: path.join("assets", "icons"), // Name of the folders; 'icons' folder go into 'assets' folder.
           },
         ],
       }),
@@ -53,19 +53,19 @@ module.exports = () => {
         // TODO: Add CSS loaders and babel to webpack.
         // Handling CSS files
         {
-          test: /\.css$/i, // match any file with .css
-          use: ["style-loader", "css-loader"], // process CSS file
+          test: /\.css$/i, // If there is a match to any file with .css,
+          use: ["style-loader", "css-loader"], // Use these loaders to process the CSS file.
         },
         // Handling JavaScript files
         {
-          test: /\.m?js$/, // match any file with .js or .mjs
-          exclude: /node_modules/, // don't process external libraries
-          // Use babel-loader to transform JavaScript code to be compatible with older browsers.
+          test: /\.m?js$/, // If there is a match to any file with .js or .mjs,
+          exclude: /node_modules/, // Don't process external libraries.
+          // Use babel-loader to transform the JavaScript code to be compatible with older browsers.
           use: {
             loader: "babel-loader",
-            // Babel options specify presets and plugins for transformation.
+            // Babel options that specify the presets and plugins for transformation.
             options: {
-              presets: ["@babel/preset-env"], // Transform modern JavaScript to an older version.
+              presets: ["@babel/preset-env"], // Transform modern JavaScript into an older version.
               plugins: [
                 "@babel/plugin-proposal-object-rest-spread", // Allow the use of object spread syntax.
                 "@babel/transform-runtime", // Transform ES6 features that require runtime support.
